ca693143bd2ac50a8963e7280e56067c6f6e48d2
Luis <Luis.CamposGarrido@axpo.com>
2023-05-16 10:37:47 +0200

    Feat: OpenAI worker

--- backend/quickstart.py
+++ backend/quickstart.py
@@ -2,12 +2,12 @@
 import os
 import openai
 openai.api_type = "azure"
-openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT") 
+openai.api_base = "https://smartcallopenai.openai.azure.com/"
 openai.api_version = "2023-03-15-preview"
-openai.api_key = os.getenv("AZURE_OPENAI_KEY")
+openai.api_key = "d93fa35ed1ee488fb9a27a0704ef0c6e"
 
 response = openai.ChatCompletion.create(
-    engine="gpt-35-turbo", # engine = "deployment_name".
+    engine="SmartCallsGPT4-test",
     messages=[
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"},
--- backend/workers.py
+++ backend/workers.py
@@ -1,23 +1,42 @@
-import azure.cognitiveservices.speech as speechsdk
+import os
+import openai
+
+openai.api_type = "azure"
+openai.api_base = "https://smartcallopenai.openai.azure.com/"
+openai.api_version = "2023-03-15-preview"
+openai.api_key = "d93fa35ed1ee488fb9a27a0704ef0c6e"
 
 
-class SpeechWorker:
+class OpenAIWorker:
     def __init__(
         self,
-        speech_api_key: str,
-        speech_service_region: str = "westeurope",
+        api_type: str = "azure",
+        api_base: str = "https://smartcallopenai.openai.azure.com/",
+        api_version: str = "2023-03-15-preview",
+        api_key: str = "d93fa35ed1ee488fb9a27a0704ef0c6e",
+        service_region: str = "francecentral",
+        engine="SmartCallsGPT4-test",
     ):
-        self.speech_api_key = speech_api_key
-        self.speech_service_region = speech_service_region
-        self.speech_config = speechsdk.SpeechConfig(
-            subscription=self.speech_api_key, region=self.speech_service_region
+        self.api_type = api_type
+        self.api_base = api_base
+        self.api_version = api_version
+        self.api_key = api_key
+        self.service_region = service_region
+        self.engine = engine
+
+    def complete(
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"},
+            {"role": "assistant", "content": "Yes, customer managed keys are supported by Azure OpenAI."},
+            {"role": "user", "content": "Do other Azure Cognitive Services support this too?"}
+        ]
+    ):
+        
+        response = openai.ChatCompletion.create(
+            engine=self.engine,
+            messages=messages,
         )
 
-    def recognize_once(self, filename) -> str:
-        audio_config = speechsdk.AudioConfig(filename=filename)
-        recognizer = speechsdk.SpeechRecognizer(
-            self.speech_config, audio_config, language="???"
-        )
-        while True:
-            # do something and get text
-            yield text
+        print(response)
+        print(response['choices'][0]['message']['content'])
